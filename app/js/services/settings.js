ac38a47945cb150623c4b696053fe3b6e1657a27
Lance Li <lyfuture@gmail.com>
2015-05-06 21:19:55 +0800

    chore: disable send from by default

--- app/js/services/settings.js
+++ app/js/services/settings.js
@@ -12,7 +12,7 @@ angular.module('cnodejs.services')
 .factory('Settings', function(ENV, $resource, $log, Storage) {
   var storageKey = 'settings';
   var settings = Storage.get(storageKey) || {
-    sendFrom: true,
+    sendFrom: false,
     saverMode: true
   };
   return {
